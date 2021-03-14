3caa5e921fc2f2336962fb42249bcc32142c9305
and-urd <andresurda@gmail.com>
2021-03-15 00:52:36 +0100

    bloqueantes - crear test

--- src/test/java/com/patterns/solid/MainTest.java
+++ src/test/java/com/patterns/solid/MainTest.java
@@ -1,10 +1,17 @@
 package com.patterns.solid;
 
+import com.patterns.Calculator;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MainTest {
 
-   // @Test
+    @Test
+    void calculadoraMultiplicarTest(){
+        Calculator calculator = new Calculator();
+        assertEquals(6, calculator.multiply(2,3));
+    }
 
 //Cambio2
 // Cambio de carpeta
