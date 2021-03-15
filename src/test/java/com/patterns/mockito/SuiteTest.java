5ebfa20c5b8c2e9d413f4c49ec0ed770f9b359f8
and-urd <andresurda@gmail.com>
2021-03-15 08:39:35 +0100

    Criticos -

--- src/test/java/com/patterns/mockito/SuiteTest.java
+++ src/test/java/com/patterns/mockito/SuiteTest.java
@@ -1,6 +1,17 @@
 package com.patterns.mockito;
 
+import com.patterns.Calculator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SuiteTest {
+
+    @Test
+    void calculculatorTest(){
+        Calculator calculator = new Calculator();
+        assertEquals(2, calculator.suma(1,1));
+    }
 
 
 }
